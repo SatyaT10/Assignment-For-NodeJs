7228baf2b73f6e279bc377300a4ea0e6b2a87c12
Satyaprakash Roy <149265635+SatyaT10@users.noreply.github.com>
2024-05-08 22:12:21 +0530

    Update userroutes.js

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -14,4 +14,18 @@ userRoutes.post('/forget-password',userControllers.forgetPassword)
 
 userRoutes.post('/set-password',auth.verifyToken,userControllers.setPassword);
 
+userRoutes.post('/post/create-post',auth.verifyToken,userControllers.createPost);
+
+userRoutes.post('/post/update-post',auth.verifyToken,userControllers.updatePost);
+
+userRoutes.get('/post',userControllers.viewAllPost);
+
+userRoutes.get('/post/mypost',auth.verifyToken,userControllers.viewMyPost);
+
+userRoutes.post('/post/delete-post',auth.verifyToken,userControllers.deleteMyPost);
+
+userRoutes.post('/post/like-post',auth.verifyToken,userControllers.likePost);
+
+userRoutes.post('/post/comment-post',auth.verifyToken,userControllers.commentPost);
+
 module.exports=userRoutes
