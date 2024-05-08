1b17a99e423e9e8e6c221a72e4d167c1384adcaa
Satyaprakash Roy <149265635+SatyaT10@users.noreply.github.com>
2024-05-08 22:11:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const userRouter = require('./routes/userroutes');
+const cors = require('cors');
 
 mongoose.connect('mongodb://127.0.0.1:27017/User-Management-System').then(() => {
     console.log("Database Connected!");
 }).catch((err) => {
     const error = new Error("Database Connection Failed!");
+    next(error)
 })
 const PORT = 8080;
 
-app.use('/api',userRouter);
+app.use('/api', userRouter);
+app.use(cors());
 
 app.get('*', (req, res, next) => {
     var err = new Error('page Not Found');
